9350614ecb3730e4a2db1085c9ff94fa93675ab7
Kyle Lacy <kylewlacy@me.com>
2012-07-11 05:13:09 -0700

    fix relative month stuff for Time#after

--- lib/timerizer.rb
+++ lib/timerizer.rb
@@ -1,8 +1,60 @@
-# Since the length of months and years aren't constant,
-# we use the averages. These also act as sort of 'magic
-# numbers' for the relative Time methods
-AVERAGE_MONTH = 2628000
-AVERAGE_YEAR  = 31540000
+class Time
+  # Since the length of months and years aren't constant,
+  # we use the averages. These also act as sort of 'magic
+  # numbers' for the relative Time methods
+  AVERAGE_MONTH = 2628000
+  AVERAGE_YEAR  = 31540000
+  def before(time)
+    Time.at(time.to_i - self.to_i)
+  end
+
+  def ago
+    self.before(Time.now)
+  end
+
+  def after(time)
+    if(self.to_i % AVERAGE_MONTH == 0)
+      new_month = (self.to_i / AVERAGE_MONTH) + time.month
+      new_year = time.year
+      while new_month > 12 do
+        new_month -= 12
+        new_year += 1
+      end
+
+      if(Time.day_exists_in_month?(time.day, new_month, new_year))
+        return Time.new(new_year, new_month, time.day, time.hour, time.min, time.sec, time.utc_offset)
+      else
+        return Time.new(new_year, new_month, Time.last_day_of_month(new_month, new_year), time.hour, time.min, time.sec, time.utc_offset)
+      end
+    elsif(self.to_i % AVERAGE_YEAR == 0)
+      # year code
+    end
+    Time.at(time.to_i + self.to_i)
+  end
+
+  def from_now
+    self.after(Time.now)
+  end
+
+  def self.day_exists_in_month?(day, month, year = Time.now.year)
+    Time.last_day_of_month(month, year) >= day
+  end
+
+  def self.last_day_of_month(month, year = Time.now.year)
+    number_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    number_of_days[1] = 29 if leap_year?(year)
+
+    number_of_days.fetch(month - 1)
+  end
+
+  def self.leap_year?(year)
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+  end
+  
+  def leap_year?
+    Time.leap_year?(self.year)
+  end
+end
 
 class Fixnum
   def seconds
@@ -26,11 +78,11 @@ class Fixnum
   end
 
   def months
-    Time.at(self * AVERAGE_MONTH)
+    Time.at(self * Time::AVERAGE_MONTH)
   end
 
   def years
-    Time.at(self * AVERAGE_YEAR)
+    Time.at(self * Time::AVERAGE_YEAR)
   end
 
   alias_method :second,  :seconds
@@ -40,22 +92,4 @@ class Fixnum
   alias_method :week,    :weeks
   alias_method :month,   :months
   alias_method :year,    :years
-end
-
-class Time
-  def before(time)
-    Time.at(time.to_i - self.to_i)
-  end
-
-  def ago
-    self.before(Time.now)
-  end
-
-  def after(time)
-    Time.at(time.to_i + self.to_i)
-  end
-
-  def from_now
-    self.after(Time.now)
-  end
 end
