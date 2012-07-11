328abbe261fdba2500f17aea876420be60aaade6
Kyle Lacy <kylewlacy@me.com>
2012-07-11 01:36:19 -0700

    Initial commit

---
+++ lib/timerizer.rb
@@ -0,0 +1,45 @@
+# Since the length of months and years aren't constant,
+# we use the averages. These also act as sort of 'magic
+# numbers' for the relative Time methods
+AVERAGE_MONTH = 2628000
+AVERAGE_YEAR  = 31540000
+
+class Fixnum
+  def seconds
+    Time.at(self)
+  end
+
+  def minutes
+    Time.at(self * 60)
+  end
+
+  def hours
+    Time.at(self * 3600)
+  end
+
+  def days
+    Time.at(self * 86400)
+  end
+
+  def weeks
+    Time.at(self * 604800)
+  end
+
+  def months
+    Time.at(self * AVERAGE_MONTH)
+  end
+
+  def years
+    Time.at(self * AVERAGE_YEAR)
+  end
+end
+
+class Time
+  def from_now
+    Time.at(self.to_i + Time.now.to_i)
+  end
+
+  def ago
+    Time.at(Time.now.to_i - self.to_i)
+  end
+end
---
+++ timerizer.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |gem|
+  gem.name        = 'timerizer'
+  gem.version     = '0.0.1'
+  gem.date        = '2012-07-11'
+  gem.summary     = 'Rails time helpers... without the Rails'
+  gem.description = 'A simple set of Rails-like time helpers'
+  gem.authors     = ['Kyle Lacy']
+  gem.email       = 'kylelacy@me.com'
+  gem.files       = ['lib/timerizer.rb']
+  gem.homepage    = 'http://github.com/kylewlacy/timerizer'
+end
