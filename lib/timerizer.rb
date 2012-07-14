888a518cec914e9b248f17fe03c8d5078fdcc2f4
Kyle Lacy <kylewlacy@me.com>
2012-07-13 21:02:09 -0700

    cut out redundant code

--- lib/timerizer.rb
+++ lib/timerizer.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class RelativeTime
   @@in_seconds = {
       :second => 1,
@@ -70,12 +72,6 @@ class RelativeTime
 end
 
 class Time
-  # Since the length of months and years aren't constant,
-  # we use the averages. These also act as sort of 'magic
-  # numbers' for the relative Time methods
-  AVERAGE_MONTH = 2628000
-  AVERAGE_YEAR  = 31540000
-
   add = instance_method(:+)
   define_method(:+) do |time|
     if(time.class == RelativeTime)
@@ -98,66 +94,8 @@ class Time
     @relative ||= false
   end
 
-  def before(time)
-    if(self.to_i % AVERAGE_MONTH == 0)
-      new_month = time.month - (self.to_i / AVERAGE_MONTH)
-      new_year = time.year
-      while new_month < 1 do
-        new_month += 12
-        new_year -= 1
-      end
-
-      if not(Time.day_exists_in_month?(time.day, new_month, new_year))
-        return Time.new(new_year, new_month, Time.days_in_month(new_month, new_year), time.hour, time.min, time.sec, time.utc_offset)
-      else
-        return Time.new(new_year, new_month, time.day, time.hour, time.min, time.sec, time.utc_offset)
-      end
-    elsif(self.to_i % AVERAGE_YEAR == 0)
-      new_year = (self.to_i / AVERAGE_YEAR) + time.year
-      if not(time.leap_day?)
-        return Time.new(new_year, time.month, time.day, time.hour, time.min, time.sec, time.utc_offset)
-      else
-        return Time.new(new_year, time.month + 1, 1, time.hour, time.min, time.sec, time.utc_offset)
-      end
-    end
-    Time.at(time.to_i - self.to_i)
-  end
-
-  def ago
-    self.before(Time.now)
-  end
-
-  def after(time)
-    if(self.to_i % AVERAGE_MONTH == 0)
-      new_month = (self.to_i / AVERAGE_MONTH) + time.month
-      new_year = time.year
-      while new_month > 12 do
-        new_month -= 12
-        new_year += 1
-      end
-
-      if not(Time.day_exists_in_month?(time.day, new_month, new_year))
-        return Time.new(new_year, new_month, Time.days_in_month(new_month, new_year), time.hour, time.min, time.sec, time.utc_offset)
-      else
-        return Time.new(new_year, new_month, time.day, time.hour, time.min, time.sec, time.utc_offset)
-      end
-    elsif(self.to_i % AVERAGE_YEAR == 0)
-      new_year = (self.to_i / AVERAGE_YEAR) + time.year
-      if not(time.leap_day?)
-        return Time.new(new_year, time.month, time.day, time.hour, time.min, time.sec, time.utc_offset)
-      else
-        return Time.new(new_year, time.month + 1, 1, time.hour, time.min, time.sec, time.utc_offset)
-      end
-    end
-    Time.at(time.to_i + self.to_i)
-  end
-
-  def from_now
-    self.after(Time.now)
-  end
-
-  def self.day_exists_in_month?(day, month, year = Time.at(0).utc.year)
-    Time.days_in_month(month, year) >= day
+  def to_date
+    Date.new(self.year, self.month, self.day)
   end
 
   def self.days_in_month(month, year = Time.at(0).utc.year)
@@ -165,47 +103,35 @@ class Time
 
     number_of_days.fetch(month - 1)
   end
-
-  def self.leap_year?(year)
-    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
-  end
-
-  def leap_year?
-    Time.leap_year?(self.year)
-  end
-
-  def leap_day?
-    self.month == 2 && self.day == 29
-  end
 end
 
 class Fixnum
   def seconds
-    Time.relative(self)
+    RelativeTime.new(self, :second)
   end
 
   def minutes
-    Time.relative(self * 60)
+    RelativeTime.new(self, :minute)
   end
 
   def hours
-    Time.relative(self * 3600)
+    RelativeTime.new(self, :hour)
   end
 
   def days
-    Time.relative(self * 86400)
+    RelativeTime.new(self, :day)
   end
 
   def weeks
-    Time.relative(self * 604800)
+    RelativeTime.new(self, :week)
   end
 
   def months
-    Time.relative(self * Time::AVERAGE_MONTH)
+    RelativeTime.new(self, :month)
   end
 
   def years
-    Time.relative(self * Time::AVERAGE_YEAR)
+    RelativeTime.new(self, :year)
   end
 
   alias_method :second,  :seconds
