bc2df4eb06bea43d2ac39b67ace3fe5ca20eb33e
Kyle Lacy <kylewlacy@me.com>
2012-07-17 10:36:10 -0700

    add yardocs and README file

---
+++ README.md
@@ -0,0 +1,27 @@
+Timerizer
+=========
+A simple Ruby time helper, just like the Rails ones. You know, without the Rails.
+
+    5.minutes.ago
+        # => 2012-07-17 10:03:03 -0700
+    5.months.ago
+        # => 2012-02-17 10:08:03 -0800
+    1.minute.from_now
+        # => 2012-07-17 10:09:03 -0700
+    5.minutes.before(Time.new(2012, 1, 1, 3, 45, 00))
+        # => 2012-01-01 03:40:00 -0800
+    1.year.after(Time.new(2012, 1, 1, 3, 45, 00))
+        # => 2013-01-01 03:45:00 -0800
+
+Requirements
+------------
+Currently, Timerizer only works in Ruby version 1.9.2 and 1.9.3
+
+Building and Installation
+-------------------------
+To build and install Timerizer, do the following:
+
+    git clone git://github.com/kylewlacy/timerizer.git
+    cd timerizer
+    rake make
+    gem install ./timerizer-{verion}.gem
--- lib/timerizer.rb
+++ lib/timerizer.rb
@@ -1,6 +1,8 @@
 require 'date'
 
+# Represents a relative amount of time. For example, `5 days`, `4 years`, and `5 years, 4 hours, 3 minutes, 2 seconds` are all RelativeTimes.
 class RelativeTime
+  # All potential units. Key is the unit name, and the value is its plural form.
   @@units = {
     :second     => :seconds,
     :minute     => :minutes,
@@ -14,6 +16,7 @@ class RelativeTime
     :millennium => :millennia
   }
 
+  # Unit values in seconds. If a unit is not present in this hash, it is assumed to be in the {@@in_months} hash.
   @@in_seconds = {
       :second => 1,
       :minute => 60,
@@ -22,6 +25,7 @@ class RelativeTime
       :week   => 604800
   }
 
+  # Unit values in months. If a unit is not present in this hash, it is assumed to be in the {@@in_seconds} hash.
   @@in_months = {
     :month      => 1,
     :year       => 12,
@@ -30,11 +34,20 @@ class RelativeTime
     :millennium => 12000
   }
 
+  # Average amount of time in a given unit. Used internally within the {#average} and {#unaverage} methods.
   @@average_seconds = {
     :month => 2629746,
     :year  => 31556952
   }
 
+  # Initialize a new instance of RelativeTime.
+  # @overload new(hash)
+  #   @param [Hash] units The base units to initialize with
+  #   @option units [Fixnum] :seconds The number of seconds
+  #   @option units [Fixnum] :months The number of months
+  # @overload new(count, unit)
+  #   @param [Fixnum] count The number of units to initialize with
+  #   @param [Symbol] unit The unit to initialize. See {@@units}
   def initialize(count = 0, unit = :second)
     if(count.is_a? Hash)
       @seconds = count[:seconds] || 0
@@ -52,12 +65,25 @@ class RelativeTime
     end
   end
 
-  def get(time)
-    return @seconds if time == :seconds
-    return @months if time == :months
+  # Return the number of base units in a RelativeTime.
+  # @param [Symbol] unit The unit to return, either :seconds or :months
+  # @return [Fixnum] The requested unit count
+  # @raise [ArgumentError] Unit requested was not :seconds or :months
+  def get(unit)
+    return @seconds if unit == :seconds
+    return @months if unit == :months
     raise ArgumentError
   end
 
+  # Determines the time between RelativeTime and the given time.
+  # @param [Time] time The initial time.
+  # @return [Time] The difference between the current RelativeTime and the given time
+  # @example 5 hours before January 1st, 2000 at noon
+  #   5.minutes.before(Time.now(2000, 1, 1, 12, 00, 00))
+  #     => 2000-01-01 11:55:00 -0800
+  # @see #ago
+  # @see #after
+  # @see #from_now
   def before(time)
     time = time.to_time - @seconds
 
@@ -80,10 +106,17 @@ class RelativeTime
     Time.at(new_time.to_i, time.nsec/1000)
   end
 
+  # Return the time between the RelativeTime and the current time.
+  # @return [Time] The difference between the current RelativeTime and Time#now
+  # @see #before
   def ago
     self.before(Time.now)
   end
 
+  # Return the time after the given time according to the current RelativeTime.
+  # @param [Time] time The starting time
+  # @return [Time] The time after the current RelativeTime and the given time
+  # @see #before
   def after(time)
     time = time.to_time + @seconds
 
@@ -107,6 +140,8 @@ class RelativeTime
     Time.at(new_time.to_i, time.nsec/1000.0)
   end
 
+  # Return the time after the current time and the RelativeTime.
+  # @return [Time] The time after the current time
   def from_now
     self.after(Time.now)
   end
@@ -136,6 +171,13 @@ class RelativeTime
     end
   end
 
+  # Average second-based units to month-based units.
+  # @return [RelativeTime] The averaged RelativeTime
+  # @example
+  #   5.weeks.average
+  #     => 1 month, 4 days, 13 hours, 30 minutes, 54 seconds
+  # @see #average!
+  # @see #unaverage
   def average
     return self unless @seconds > 0
 
@@ -147,6 +189,8 @@ class RelativeTime
     })
   end
 
+  # Destructively average second-based units to month-based units.
+  # @see #average
   def average!
     averaged = self.average
     @seconds = averaged.get(:seconds)
@@ -154,12 +198,21 @@ class RelativeTime
     self
   end
 
+  # Average month-based units to second-based units.
+  # @return [RelativeTime] the unaveraged RelativeTime.
+  # @example
+  #   1.month.unaverage
+  #     => 4 weeks, 2 days, 10 hours, 29 minutes, 6 seconds
+  # @see #average
+  # @see #unaverage!
   def unaverage
     seconds = @@average_seconds[:month] * @months
     seconds += @seconds
     RelativeTime.new({:seconds => seconds})
   end
 
+  # Destructively average month-based units to second-based units.
+  # @see #unaverage
   def unaverage!
     unaveraged = self.average
     @seconds = unaverage.get(:seconds)
@@ -167,6 +220,9 @@ class RelativeTime
     self
   end
 
+  # Add two {RelativeTime}s together.
+  # @raise ArgumentError Argument isn't a {RelativeTime}
+  # @see #-
   def +(time)
     raise ArgumentError unless time.is_a?(RelativeTime)
     RelativeTime.new({
@@ -175,6 +231,9 @@ class RelativeTime
     })
   end
 
+  # Find the difference between two {RelativeTime}s.
+  # @raise ArgumentError Argument isn't a {RelativeTime}
+  # @see #+
   def -(time)
     raise ArgumentError unless time.is_a?(RelativeTime)
     RelativeTime.new({
@@ -183,22 +242,10 @@ class RelativeTime
     })
   end
 
-  def *(factor)
-    raise ArgumentError unless factor.is_a?(Numeric)
-    RelativeTime.new({
-      :seconds => @seconds * factor,
-      :months => @months * factor
-    })
-  end
-
-  def /(factor)
-    raise ArgumentError unless factor.is_a?(Numeric)
-    RelativeTime.new({
-      :seconds => @seconds / factor,
-      :months => @months / factor
-    })
-  end
-
+  # Convert {RelativeTime} to a human-readable format.
+  # @example
+  #   (14.months 49.hours).to_s
+  #     => 2 years, 2 months, 3 days, 1 hour
   def to_s
     times = [] 
 
@@ -212,7 +259,7 @@ class RelativeTime
     end.reverse.join(', ')
   end
 end
-
+# {Time} class monkeywrenched with {RelativeTime} support.
 class Time
   add = instance_method(:+)
   define_method(:+) do |time|
@@ -232,16 +279,29 @@ class Time
     end
   end
 
+  # Convert {Time} to {Date}.
+  # @return [Date] {Time} as {Date}
+  # @example
+  #   Time.new(2000, 1, 1, 12, 30).to_date
+  #     => #<Date: 2000-01-01 ((2451545j,0s,0n),+0s,2299161j)>
   def to_date
     Date.new(self.year, self.month, self.day)
   end
 
+  # Convert self to {Time}.
+  # @see Date#to_time
   def to_time
     self
   end
 end
 
+# {Date} class monkeywrenched with {RelativeTime} helpers.
 class Date
+  # Return the number of days in a given month.
+  # @return [Fixnum] Number of days in the month of the {Date}.
+  # @example
+  #   Date.new(2000, 2).days_in_month
+  #     => 29
   def days_in_month
     days_in_feb = (not self.leap?) ? 28 : 29
     number_of_days = [
@@ -252,11 +312,18 @@ class Date
     number_of_days.fetch(self.month - 1)
   end
 
+  # Return self as {Date}.
+  # @see Time#to_date
   def to_date
     self
   end
 end
 
+# Monkeywrenched {Fixnum} class enabled to return {RelativeTime} objects.
+# @example
+#   5.minutes
+#     => 5 minutes
+# @see RelativeTime @@units
 class Fixnum
   units  = RelativeTime.class_variable_get(:@@units)
   units.each do |unit, plural|
