9595d6d0fae35628d343a24a820c5a2b9410fb70
Kyle Lacy <kylewlacy@me.com>
2012-07-17 08:39:13 -0700

    add more specs

--- spec/timerizer_spec.rb
+++ spec/timerizer_spec.rb
@@ -28,14 +28,70 @@ describe RelativeTime do
       1.month.after(end_of_january).should == Time.new(2000, 2, 29, 3, 45, 00)
     end
   end
+
+  it "can average from second units to month units" do
+    five_weeks = {
+      :seconds => 3024000,
+      :average => {:seconds => 394254, :months => 1}
+    }
+
+    5.weeks.get(:seconds).should == five_weeks[:seconds]
+
+    average = 5.weeks.average!
+    average.get(:seconds).should == five_weeks[:average][:seconds]
+    average.get(:months).should == five_weeks[:average][:months]
+  end
+
+  it "can unaverage from month units to second units" do
+    two_months = {
+      :months => 2,
+      :unaverage => {:seconds => 5259492, :months => 0}
+    }
+
+    2.months.get(:months).should == two_months[:months]
+
+    unaverage = 2.months.unaverage!
+
+    unaverage.get(:seconds).should == two_months[:unaverage][:seconds]
+    unaverage.get(:months).should == two_months[:unaverage][:months]
+  end
 end
 
 describe Time do
-  
+  it "can be added or subtracted to RelativeTime" do
+    time = Time.new(2000, 1, 1, 3, 45, 00)
+    (time + 5.minutes).should == Time.new(2000, 1, 1, 3, 50, 00)
+    (time - 5.minutes).should == Time.new(2000, 1, 1, 3, 40, 00)
+  end
+
+  it "can be converted to a Date object" do
+    time = Time.new(2000, 1, 1, 11, 59, 00)
+    time.to_date.should == Date.new(2000, 1, 1)
+  end
 end
 
 describe Date do
+  it "can be converted to a Time object" do
+    date = Date.new(2000, 1, 1)
+    date.to_time.should == Time.new(2000, 1, 1)
+  end
+
+  it "returns the number of days in a month" do
+    Date.new(2000, 1).days_in_month.should == 31
+    Date.new(2000, 2).days_in_month.should == 29
+    Date.new(2001, 2).days_in_month.should == 28
+  end
 end
 
 describe Fixnum do
+  it "makes RelativeTime objects" do
+    1.minute.get(:seconds).should == 60
+    3.hours.get(:seconds).should == 10800
+    5.days.get(:seconds).should == 432000
+    4.years.get(:months).should == 48
+
+    relative_time = 1.second 2.minutes 3.hours 4.days 5.weeks 6.months 7.years
+    relative_time.get(:seconds).should == 3380521
+    relative_time.get(:months).should == 90
+  end
 end
