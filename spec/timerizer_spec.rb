1231600cf3f071bbdf6b8d53d89f4208acb9910d
Kyle Lacy <kylewlacy@me.com>
2017-09-23 15:24:48 -0700

    Replace all uses of `should` with `expect` in specs

--- spec/timerizer_spec.rb
+++ spec/timerizer_spec.rb
@@ -7,36 +7,40 @@ RSpec.describe RelativeTime do
     end
 
     it "calculates a new time before itself" do
-      5.minutes.before(@time).should == Time.new(2000, 1, 1, 3, 40, 00)
-      5.months.before(@time).should == Time.new(1999, 8, 1, 3, 45, 00)
+      expect(5.minutes.before(@time)).to eq(Time.new(2000, 1, 1, 3, 40, 00))
+      expect(5.months.before(@time)).to eq(Time.new(1999, 8, 1, 3, 45, 00))
     end
 
     it "calculates a new time after itself" do
-      5.minutes.after(@time).should == Time.new(2000, 1, 1, 3, 50, 00)
-      5.months.after(@time).should == Time.new(2000, 6, 1, 3, 45, 00)
+      expect(5.minutes.after(@time)).to eq(Time.new(2000, 1, 1, 3, 50, 00))
+      expect(5.months.after(@time)).to eq(Time.new(2000, 6, 1, 3, 45, 00))
     end
 
     it "properly handles large periods of time" do
-      65.months.before(@time).should == Time.new(1994, 8, 1, 3, 45, 00)
-      65.months.after(@time).should == Time.new(2005, 6, 1, 3, 45, 00)
+      expect(65.months.before(@time)).to eq(Time.new(1994, 8, 1, 3, 45, 00))
+      expect(65.months.after(@time)).to eq(Time.new(2005, 6, 1, 3, 45, 00))
     end
   end
 
   context "given an odd time case" do
     it "properly calculates the time before it" do
       end_of_march = Time.new(2000, 3, 31, 3, 45, 00)
-      1.month.before(end_of_march).should == Time.new(2000, 2, 29, 3, 45, 00)
+      expect(
+        1.month.before(end_of_march)
+      ).to eq(Time.new(2000, 2, 29, 3, 45, 00))
     end
 
     it "properly calculates the time after it" do
       end_of_january = Time.new(2000, 1, 31, 3, 45, 00)
-      1.month.after(end_of_january).should == Time.new(2000, 2, 29, 3, 45, 00)
+      expect(
+        1.month.after(end_of_january)
+      ).to eq(Time.new(2000, 2, 29, 3, 45, 00))
     end
   end
 
-  context "#to_wall" do
+  describe "#to_wall" do
     it "calculates an equivalent WallClock time" do
-      (5.hours 30.minutes).to_wall.should == WallClock.new(5, 30)
+      expect((5.hours 30.minutes).to_wall).to eq(WallClock.new(5, 30))
     end
 
     it "raises an error for times beyond 24 hours" do
@@ -54,26 +58,35 @@ RSpec.describe RelativeTime do
     end
   end
 
-  context "#to_s" do
+  describe "#to_s" do
     it "converts all units into a string" do
-      (1.hour 3.minutes 4.seconds).to_s.should ==
-        "1 hour, 3 minutes, 4 seconds"
-      (1.year 3.months 4.days).to_s(:long).should ==
-        "1 year, 3 months, 4 days"
+      expect(
+        (1.hour 3.minutes 4.seconds).to_s
+      ).to eq("1 hour, 3 minutes, 4 seconds")
+
+      expect(
+        (1.year 3.months 4.days).to_s(:long)
+      ).to eq("1 year, 3 months, 4 days")
     end
 
     it "converts units into a micro syntax" do
-      (1.hour 3.minutes 4.seconds).to_s(:micro).should ==
-        "1h"
-      (1.year 3.months 4.days).to_s(:micro).should ==
-        "1y"
+      expect(
+        (1.hour 3.minutes 4.seconds).to_s(:micro)
+      ).to eq("1h")
+
+      expect(
+        (1.year 3.months 4.days).to_s(:micro)
+      ).to eq("1y")
     end
 
     it "converts units into a medium syntax" do
-      (1.hour 3.minutes 4.seconds).to_s(:short).should ==
-        "1hr 3min"
-      (1.year 3.months 4.days).to_s(:short).should ==
-        "1yr 3mn"
+      expect(
+        (1.hour 3.minutes 4.seconds).to_s(:short)
+      ).to eq("1hr 3min")
+
+      expect(
+        (1.year 3.months 4.days).to_s(:short)
+      ).to eq("1yr 3mn")
     end
   end
 
@@ -83,11 +96,11 @@ RSpec.describe RelativeTime do
       :average => {:seconds => 394254, :months => 1}
     }
 
-    5.weeks.get(:seconds).should == five_weeks[:seconds]
+    expect(5.weeks.get(:seconds)).to eq(five_weeks[:seconds])
 
     average = 5.weeks.average!
-    average.get(:seconds).should == five_weeks[:average][:seconds]
-    average.get(:months).should == five_weeks[:average][:months]
+    expect(average.get(:seconds)).to eq(five_weeks[:average][:seconds])
+    expect(average.get(:months)).to eq(five_weeks[:average][:months])
   end
 
   it "can unaverage from month units to second units" do
@@ -96,61 +109,66 @@ RSpec.describe RelativeTime do
       :unaverage => {:seconds => 5259492, :months => 0}
     }
 
-    2.months.get(:months).should == two_months[:months]
+    expect(2.months.get(:months)).to eq(two_months[:months])
 
     unaverage = 2.months.unaverage!
 
-    unaverage.get(:seconds).should == two_months[:unaverage][:seconds]
-    unaverage.get(:months).should == two_months[:unaverage][:months]
+    expect(unaverage.get(:seconds)).to eq(two_months[:unaverage][:seconds])
+    expect(unaverage.get(:months)).to eq(two_months[:unaverage][:months])
   end
 
   it "can compare two RelativeTimes" do
-    1.minute.should == 1.minute
-    1.minute.should_not == 1.hour
+    expect(1.minute).to eq(1.minute)
+    expect(1.minute).not_to eq(1.hour)
   end
 end
 
-describe WallClock do
+RSpec.describe WallClock do
   it "can be created" do
     WallClock.new(12, 30, :pm)
     WallClock.new(23, 30)
   end
 
   it "can be created from a string" do
-    WallClock.from_string("9:00 PM").should == WallClock.new(9, 00, :pm)
-    WallClock.from_string("13:00").should == WallClock.new(13, 00)
-    WallClock.from_string("12:00 PM").should == WallClock.new(12, 00, :pm)
-    WallClock.from_string("11:00:01 PM").should == WallClock.new(11, 00, 01, :pm)
-    WallClock.from_string("23:34:45").should == WallClock.new(23, 34, 45)
+    expect(WallClock.from_string("9:00 PM")).to eq(WallClock.new(9, 00, :pm))
+    expect(WallClock.from_string("13:00")).to eq(WallClock.new(13, 00))
+    expect(WallClock.from_string("12:00 PM")).to eq(WallClock.new(12, 00, :pm))
+    expect(WallClock.from_string("23:34:45")).to eq(WallClock.new(23, 34, 45))
+
+    expect(
+      WallClock.from_string("11:00:01 PM")
+    ).to eq(WallClock.new(11, 00, 01, :pm))
   end
 
   it "can apply a time on a day" do
     date = Date.new(2000, 1, 1)
-    WallClock.new(9, 00, :pm).on(date).should == Time.new(2000, 1, 1, 21)
+    expect(WallClock.new(9, 00, :pm).on(date)).to eq(Time.new(2000, 1, 1, 21))
   end
 
   it "can be initialized from a hash of values" do
     date = Date.new(2000, 1, 1)
-    WallClock.new(:second => 30*60).on(date).should == Time.new(2000, 1, 1, 0, 30)
+    expect(
+      WallClock.new(:second => 30*60).on(date)
+    ).to eq(Time.new(2000, 1, 1, 0, 30))
   end
 
-  it "can be converted to an from an integer" do
+  it "can be converted from an integer" do
     time = WallClock.new(21, 00)
-    WallClock.new(time.to_i).should == WallClock.new(9, 00, :pm)
+    expect(WallClock.new(time.to_i)).to eq(WallClock.new(9, 00, :pm))
   end
 
   it "can return its components" do
     time = WallClock.new(5, 35, 45, :pm)
-    time.hour.should == 17
-    time.hour(:twenty_four_hour).should == 17
-    time.hour(:twelve_hour).should == 5
-    time.minute.should == 35
-    time.second.should == 45
-    time.meridiem.should == :pm
+    expect(time.hour).to eq(17)
+    expect(time.hour(:twenty_four_hour)).to eq(17)
+    expect(time.hour(:twelve_hour)).to eq(5)
+    expect(time.minute).to eq(35)
+    expect(time.second).to eq(45)
+    expect(time.meridiem).to eq(:pm)
 
-    time.in_seconds.should == (17*3600) + (35*60) + 45
-    time.in_minutes.should == (17*60) + 35
-    time.in_hours.should == 17
+    expect(time.in_seconds).to eq((17*3600) + (35*60) + 45)
+    expect(time.in_minutes).to eq((17*60) + 35)
+    expect(time.in_hours).to eq(17)
 
     expect do
       time.hour(:thirteen_hour)
@@ -172,114 +190,125 @@ describe WallClock do
   end
 
   it "can be converted to RelativeTime" do
-    WallClock.new(5, 30, 27, :pm).to_relative.should ==
-      (17.hours 30.minutes 27.seconds)
+    expect(
+      WallClock.new(5, 30, 27, :pm).to_relative
+    ).to eq(17.hours 30.minutes 27.seconds)
   end
 
-  context "#to_s" do
+  describe "#to_s" do
     before do
       @time = WallClock.new(5, 30, 27, :pm)
     end
 
     it "can be converted to a 12-hour time string" do
-      @time.to_s.should == "5:30:27 PM"
-      @time.to_s(:twelve_hour).should == "5:30:27 PM"
-      @time.to_s(:twelve_hour, :use_seconds => false).should == "5:30 PM"
-      @time.to_s(:twelve_hour, :include_meridiem => false).should == "5:30:27"
-      @time.to_s(
-        :twelve_hour,
-        :include_meridiem => false,
-        :use_seconds => false
-      ).should == "5:30"
+      expect(@time.to_s).to eq("5:30:27 PM")
+      expect(@time.to_s(:twelve_hour)).to eq("5:30:27 PM")
+      expect(@time.to_s(:twelve_hour, :use_seconds => false)).to eq("5:30 PM")
+      expect(@time.to_s(:twelve_hour, :include_meridiem => false)).to eq("5:30:27")
+
+      expect(
+        @time.to_s(
+          :twelve_hour,
+          :include_meridiem => false,
+          :use_seconds => false
+        )
+      ).to eq("5:30")
     end
 
     it "can be converted to a 24-hour time string" do
-      @time.to_s(:twenty_four_hour).should == "17:30:27"
-      @time.to_s(:twenty_four_hour, :use_seconds => false).should == "17:30"
+      expect(@time.to_s(:twenty_four_hour)).to eq("17:30:27")
+      expect(
+        @time.to_s(:twenty_four_hour, :use_seconds => false)
+      ).to eq("17:30")
     end
 
     it "zero-pads units" do
       time = WallClock.new(0, 00, 00)
-      time.to_s(:twelve_hour).should == "12:00:00 PM"
-      time.to_s(:twenty_four_hour).should == "0:00:00"
+      expect(time.to_s(:twelve_hour)).to eq("12:00:00 PM")
+      expect(time.to_s(:twenty_four_hour)).to eq("0:00:00")
 
-      time.to_s(:twelve_hour, :use_seconds => false).should == "12:00 PM"
-      time.to_s(:twenty_four_hour, :use_seconds => false).should == "0:00"
+      expect(time.to_s(:twelve_hour, :use_seconds => false)).to eq("12:00 PM")
+      expect(time.to_s(:twenty_four_hour, :use_seconds => false)).to eq("0:00")
     end
   end
 end
 
-describe Time do
+RSpec.describe Time do
   it "can be added or subtracted to RelativeTime" do
     time = Time.new(2000, 1, 1, 3, 45, 00)
-    (time + 5.minutes).should == Time.new(2000, 1, 1, 3, 50, 00)
-    (time - 5.minutes).should == Time.new(2000, 1, 1, 3, 40, 00)
+    expect(time + 5.minutes).to eq(Time.new(2000, 1, 1, 3, 50, 00))
+    expect(time - 5.minutes).to eq(Time.new(2000, 1, 1, 3, 40, 00))
   end
 
   it "can be converted to a Date object" do
     time = Time.new(2000, 1, 1, 11, 59, 00)
-    time.to_date.should == Date.new(2000, 1, 1)
+    expect(time.to_date).to eq(Date.new(2000, 1, 1))
   end
 
   it "calculates the time between two Times" do
     time = 1.minute.ago
-    Time.until(1.minute.from_now).in_seconds.should be_within(1.0).of(60)
-    Time.since(1.hour.ago).in_seconds.should be_within(1.0).of(3600)
+    expect(Time.until(1.minute.from_now).in_seconds).to be_within(1.0).of(60)
+    expect(Time.since(1.hour.ago).in_seconds).to be_within(1.0).of(3600)
 
-    Time.between(1.minute.ago, 2.minutes.ago).in_seconds.should be_within(1.0).of(60)
-    Time.between(Date.yesterday, Date.tomorrow).in_seconds.should be_within(1.0).of(2.days.in_seconds)
+    expect(
+      Time.between(1.minute.ago, 2.minutes.ago).in_seconds
+    ).to be_within(1.0).of(60)
 
-    lambda do
+    expect(
+      Time.between(Date.yesterday, Date.tomorrow).in_seconds
+    ).to be_within(1.0).of(2.days.in_seconds)
+
+    expect do
       Time.until(1.minute.ago)
-    end.should raise_error(Time::TimeIsInThePastError)
+    end.to raise_error(Time::TimeIsInThePastError)
 
-    lambda do
+    expect do
       Time.since(Date.tomorrow)
-    end.should raise_error(Time::TimeIsInTheFutureError)
+    end.to raise_error(Time::TimeIsInTheFutureError)
   end
 
   it "can be converted to a WallClock time" do
     time = Time.new(2000, 1, 1, 17, 58, 04)
-    time.to_wall.should == WallClock.new(5, 58, 04, :pm)
+    expect(time.to_wall).to eq(WallClock.new(5, 58, 04, :pm))
   end
 end
 
-describe Date do
+RSpec.describe Date do
   it "can be converted to a Time object" do
     date = Date.new(2000, 1, 1)
-    date.to_time.should == Time.new(2000, 1, 1)
+    expect(date.to_time).to eq(Time.new(2000, 1, 1))
   end
 
   it "returns the number of days in a month" do
-    Date.new(2000, 1).days_in_month.should == 31
-    Date.new(2000, 2).days_in_month.should == 29
-    Date.new(2001, 2).days_in_month.should == 28
+    expect(Date.new(2000, 1).days_in_month).to eq(31)
+    expect(Date.new(2000, 2).days_in_month).to eq(29)
+    expect(Date.new(2001, 2).days_in_month).to eq(28)
   end
 
   it "returns the date yesterday and tomorrow" do
     yesterday = 1.day.ago.to_date
     tomorrow = 1.day.from_now.to_date
 
-    Date.yesterday.should == yesterday
-    Date.tomorrow.should == tomorrow
+    expect(Date.yesterday).to eq(yesterday)
+    expect(Date.tomorrow).to eq(tomorrow)
   end
 
   it "returns the time on a given date" do
      date = Date.new(2000, 1, 1)
      time = WallClock.new(5, 00, :pm)
-     date.at(time).should == Time.new(2000, 1, 1, 17)
+     expect(date.at(time)).to eq(Time.new(2000, 1, 1, 17))
   end
 end
 
-describe Integer do
+RSpec.describe Integer do
   it "makes RelativeTime objects" do
-    1.minute.get(:seconds).should == 60
-    3.hours.get(:seconds).should == 10800
-    5.days.get(:seconds).should == 432000
-    4.years.get(:months).should == 48
+    expect(1.minute.get(:seconds)).to eq(60)
+    expect(3.hours.get(:seconds)).to eq(10800)
+    expect(5.days.get(:seconds)).to eq(432000)
+    expect(4.years.get(:months)).to eq(48)
 
     relative_time = 1.second 2.minutes 3.hours 4.days 5.weeks 6.months 7.years
-    relative_time.get(:seconds).should == 3380521
-    relative_time.get(:months).should == 90
+    expect(relative_time.get(:seconds)).to eq(3380521)
+    expect(relative_time.get(:months)).to eq(90)
   end
 end
