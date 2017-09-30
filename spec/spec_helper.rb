6086b25d6435f53f9405ca8aa89c2384f5aaa53c
Kyle Lacy <kylewlacy@me.com>
2017-09-30 15:26:30 -0700

    Disable global rspec DSL

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  config.expose_dsl_globally = false
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
